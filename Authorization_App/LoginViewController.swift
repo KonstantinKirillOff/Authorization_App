268c40102740a181fdf9e3de511c23dbf834dec4
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-08-25 22:03:43 +0300

    add logic for two screens

--- Authorization_App/LoginViewController.swift
+++ Authorization_App/LoginViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
     @IBOutlet weak var forgetUserNameButton: UIButton!
     @IBOutlet weak var forgetPasswordButton: UIButton!
     
@@ -27,7 +31,54 @@ class ViewController: UIViewController {
         self.view.endEditing(true)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeSV = segue.destination as? WelcomeViewController else { return }
+        welcomeSV.userName = userNameTF.text
+    }
+    
+    @IBAction func logOf(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    
+    @IBAction func forgetUserNamePressed() {
+        showAlert(title: "Ooops!", message: "Your name is Alex 😉", false)
+    }
+    
+    
+    @IBAction func forgetPasswordPressed() {
+        showAlert(title: "Ooops!", message: "Your password is 123 🤫", false)
+    }
+    
+    
+    @IBAction func loginButtonPressed() {
+        if let userName = userNameTF.text, let password = passwordTF.text {
+            checkData(userName: userName, password: password)
+        }
+        
+    }
+    
+}
 
-
+// MARK: - Private function
+extension ViewController {
+    private func showAlert(title: String, message: String, _ invalidData: Bool) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel) {_ in
+            if invalidData {
+                self.passwordTF.text = ""
+            }
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func checkData(userName: String, password: String) {
+        if ( userName != "Alex" || password != "123" ) {
+            showAlert(title: "Invalid login or password", message: "Please, enter correct ligin and password", true)
+        }
+    }
+    
 }
 
--- Authorization_App/WelcomeViewController.swift
+++ Authorization_App/WelcomeViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var userName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        guard let userName = self.userName else { return }
+        welcomeLabel.text = "Welcome, \(userName)"
     }
 
 }
