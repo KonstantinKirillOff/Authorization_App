5f65384845d458dfb333edfeb2a3aa4f9a138f48
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-08-25 17:49:34 +0300

    add constraints for main screen

---
+++ Authorization_App/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Authorization_App
+//
+//  Created by Константин Кириллов on 25.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var forgetUserNameButton: UIButton!
+    @IBOutlet weak var forgetPasswordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        forgetPasswordButton.titleLabel!.adjustsFontSizeToFitWidth = true
+        forgetPasswordButton.titleLabel!.minimumScaleFactor = 0.1
+        forgetPasswordButton.layer.cornerRadius = 10
+        
+        forgetUserNameButton.titleLabel!.adjustsFontSizeToFitWidth = true
+        forgetUserNameButton.titleLabel!.minimumScaleFactor = 0.1
+        forgetUserNameButton.layer.cornerRadius = 10
+        
+        
+    }
+
+
+}
+
